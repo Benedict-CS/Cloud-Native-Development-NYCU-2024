387335aae2320bce97944ac206add3f4cbef82a0
Benedict-CS <benedicttiong.cs@gmail.com>
2024-06-03 12:40:08 +0000

    Apply code formatting

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -7,9 +7,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json-summary', 'json'], // list of reporters
       reportsDirectory: './coverage', // target directory for coverage reports
-      exclude: [
-        'coverage-compare/**' 
-      ],
+      exclude: ['coverage-compare/**'],
       thresholds: {
         branches: 55.55,
         autoUpdate: true
