8a949dfa0c9727e5b039d750d7419c151f812880
Benedict-CS <benedicttiong.cs@gmail.com>
2024-06-03 20:18:37 +0800

    update ci.yaml

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -5,14 +5,14 @@ export default defineConfig({
     pool: 'forks',
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html', 'json'], // list of reporters
+      reporter: ['text', 'json-summary','json'], // list of reporters
       reportsDirectory: './coverage', // target directory for coverage reports
       exclude: [
         'coverage-compare/**' // Exclude specific file
         // You can add more patterns to exclude other files or directories
       ],
       thresholds: {
-        branches: 0,
+        branches: 55.55,
         autoUpdate: true
       }
     }
