c64d5a7ede0da6e9fcd36d0cebd6be0e1c9a893a
Benedict-CS <benedicttiong.cs@gmail.com>
2024-06-14 20:59:39 +0800

    test cd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -44,7 +44,7 @@ const App = () => {
 
   return (
     <main className="App">
-          <h1>My Todos argocd test for the demo</h1>
+          <h1>My Todos argocd demo</h1>
       <AddTodo saveTodo={handleSaveTodo} />
       {todos.map((todo: ITodo) => (
         <TodoItem key={todo.id} updateTodo={handleUpdateTodo} deleteTodo={handleDeleteTodo} todo={todo} />
